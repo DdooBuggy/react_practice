26000d58934b8b9d2b118f8f398a2b7ec1ab4a51
DdooBuggy <yhiyo03@gmail.com>
2022-04-13 10:12:15 +0900

    ~#6 effects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome!</h1>
-      <Button text={"Continue"} />
-      <span>nice to meet you!</span>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
